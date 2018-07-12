82bf71a467219311caed4083794e840e3ec17f84
Jitender Singh <jitender.singh@tabcorp.com.au>
2018-07-12 10:22:37 +1000

    Adding First Componet

---
+++ src/MenuBar/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class MenuBar extends Component {
+    render() {
+        return (
+            <div className="menu-bar">
+                <header className="menu-bar-header">
+                    <h1 className="menu-bar-title">Menu Bar</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default MenuBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MenuBar from './MenuBar';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MenuBar />, document.getElementById('root'));
 registerServiceWorker();
